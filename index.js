291384f1f164a12cab6b0ae9dbf97ad9e38f0f0b
Carl Jones <jonescr2udmercy.edu>
2021-09-06 16:43:12 -0400

    push

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 
 
-document.getElementById('thing').addEventListener('click', function () {
-  var x = document.getElementById('test').value * 5; 
-  document.getElementById('output').innerHTML += x;
+
+document.getElementById('Total').addEventListener('click', function () {
+  var sum = 0;
+  $('.GreenInput').each(function(){
+  sum += parseFloat(this.value);
+  })
+  document.getElementById('GreenOutput').innerHTML = sum;
 })
 
 document.getElementById('AddMore').addEventListener('click', function () {
-  document.getElementById('MoreRows').innerHTML += '<div class="row"> <div class="col"> <input> </div> <div class="col"> <input> </div><div class="col"> <input> </div> <div class="col"> <input></div><div class="col"> <input></div></div>'
+  document.getElementById('MoreRows').innerHTML += '<div class="row"> <div class="col"> <input class="GreenInput" value="0"> </div> <div class="col"> <input value="0"> </div><div class="col"> <input value="0"> </div> <div class="col"> <input value="0"></div><div class="col"> <input value="0"></div></div>'
+  //this needs to be updated to use .appendChild() instead of .innerHTML https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_node_appendchild
 })
